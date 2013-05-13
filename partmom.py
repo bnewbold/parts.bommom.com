f786a27b7f1cd81062c1a40d7e99ee3a4094cb99
bnewbold <bnewbold@robocracy.org>
2013-05-13 17:11:48 +0000

    fix freezer mimetype problem

--- partmom.py
+++ partmom.py
@@ -15,11 +15,11 @@ app.config.from_object(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/xilinx/spartan6', methods=['GET'])
+@app.route('/xilinx/spartan6/', methods=['GET'])
 def xilinx_spartan6():
     return render_template('grid.html', grid=xilinx.spartan6_grid)
 
-@app.route('/xilinx/zynq7000', methods=['GET'])
+@app.route('/xilinx/zynq7000/', methods=['GET'])
 def xilinx_zynq7000():
     return render_template('grid.html', grid=xilinx.zynq7000_grid)
 
